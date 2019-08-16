06cbde5b4660bcc44d30e4b214038a28ecc2dac4
lzx <nie_bayes@foxmail.com>
2019-08-16 20:57:46 +0800

    init ch3

---
+++ ch3/useEigen/eigenMatrix.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+#define MATRIX_SIZE 50
+
+
+int main(int argc, char **argv) {
+    Matrix<float, 2, 3> matrix_23;
+    cout << matrix_23 << endl;
+}
+
